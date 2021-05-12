73ac10a342740ce08e30406c411e139ef41dafda
Matt Stallman <matthew.stallman@hcl.com>
2021-05-12 16:11:19 -0500

    added basic catalog details component

--- inventory-app/src/app/catalog/catalog-list/catalog-list.component.ts
+++ inventory-app/src/app/catalog/catalog-list/catalog-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Package } from '../package.model';
 
 @Component({
   selector: 'app-catalog-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalog-list.component.css']
 })
 export class CatalogListComponent implements OnInit {
+  packages: Package[] = [
+    new Package('A test package', 'Just testing', '../assets/skincare.png'),
+    new Package('A test package', 'Just testing', '../assets/skincare.png')
+  ];
 
   constructor() { }
 
